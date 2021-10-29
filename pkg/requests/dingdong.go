699c0e0da4221635f920fec2397620e226a98a2f
wesley <wesley_lai@mail.chungyo.net>
2021-10-29 14:38:50 +0800

    feature/增加註釋

--- pkg/requests/dingdong.go
+++ pkg/requests/dingdong.go
@@ -6,13 +6,13 @@ import (
 	"sync"
 )
 
-// Prepare _
+// Prepare 訪問所需
 type Prepare struct {
 	Amount int
 	Dest   string
 }
 
-// Travel _
+// Travel 開始訪問
 func (info Prepare) Travel(wg *sync.WaitGroup) {
 	_, err := http.Get(info.Dest)
 	if err != nil {
